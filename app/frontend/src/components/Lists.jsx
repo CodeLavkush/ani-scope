52eb243457b67310a1aebe748784a352e00cc177
CodeLavkush <mauryalavkush19@gmail.com>
2026-04-27 16:23:01 +0530

    Add Menu sidebar to lists page

--- app/frontend/src/components/Lists.jsx
+++ app/frontend/src/components/Lists.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import smallLogo from "../assets/smallLogo.png"
+import { Menu, X } from "lucide-react"
+import { Link } from 'react-router-dom'
 
 function Lists() {
+    const [menuVisible, setMenuVisible] = useState(false)
+
+    const handleMenu = () => {
+        setMenuVisible(!menuVisible)
+    }
     return (
         <div className='w-screen h-screen overflow-hidden'>
             {/* large screens */}
-            <div className='hidden lg:flex bg-primary w-full h-full p-10'>
+            <div className='hidden lg:flex bg-primary w-full h-full p-10 relative'>
+                <div className={`max-w-100 h-full p-2 grid grid-rows-8 bg-primary absolute right-0 transition-all z-1 ${menuVisible ? "flex" : "hidden"}`}>
+                    {["Home", "About", "Submission", "Lists"].map((value, index) => (
+                        <Link to={`/${value.toLowerCase()}`} key={index} className='row-span-1 px-8 content-center text-xl font-poppins tracking-wider uppercase font-bold border-b-2 cursor-pointer active:bg-accent active:text-white'>{value}</Link>
+                    ))}
+                    <div className='row-span-2 flex justify-around items-center gap-10'>
+                        {["Login", "Register"].map((value, index) => (
+                            <Link to={`/${value.toLowerCase()}`} key={index} className='text-xl bg-accent text-white font-poppins font-medium tracking-wider px-8 py-4 rounded-md active:bg-bg active:text-black'>{value}</Link>
+                        ))}
+                    </div>
+                    <div className='row-span-2'>
+                        <button onClick={handleMenu} className='w-full h-full flex justify-start items-center cursor-pointer'>
+                            <X className='text-accent text-2xl font-bold h-12 w-12' />
+                        </button>
+                    </div>
+                </div>
                 <div className='bg-bg w-full h-full rounded-xl grid grid-rows-8'>
-                    <div className='row-span-1 flex justify-start items-center px-10'>
-                        <img src={smallLogo} alt="logo" className='object-cover w-40' />
+                    <div className='row-span-1 flex justify-around items-center px-10'>
+                        <div>
+                            <img src={smallLogo} alt="logo" className='object-cover w-40' />
+                        </div>
+                        <div>
+                            <button onClick={handleMenu} className='w-full h-full flex justify-center items-center cursor-pointer'>
+                                <Menu className='text-accent text-2xl font-bold h-12 w-12' />
+                            </button>
+                        </div>
                     </div>
                     <div className='row-span-7 flex flex-col justify-center gap-4 p-4 overflow-y-auto pt-12'>
                         {[...Array(100)].map((_, i) => (
@@ -36,10 +65,32 @@ function Lists() {
 
 
             {/* smaller screens */}
-            <div className='lg:hidden flex bg-primary w-full h-full p-10'>
+            <div className='lg:hidden flex bg-primary w-full h-full p-10 relative'>
+                <div className={`md:max-w-100 w-full h-full p-2 grid grid-rows-8 bg-primary absolute right-0 transition-all z-1 ${menuVisible ? "flex" : "hidden"}`}>
+                    {["Home", "About", "Submission", "Lists"].map((value, index) => (
+                        <Link to={`/${value.toLowerCase()}`} key={index} className='row-span-1 px-8 content-center text-xl font-poppins tracking-wider uppercase font-bold border-b-2 cursor-pointer active:bg-accent active:text-white'>{value}</Link>
+                    ))}
+                    <div className='row-span-2 flex justify-around items-center gap-10'>
+                        {["Login", "Register"].map((value, index) => (
+                            <Link to={`/${value.toLowerCase()}`} key={index} className='text-xl bg-accent text-white font-poppins font-medium tracking-wider px-8 py-4 rounded-md active:bg-bg active:text-black'>{value}</Link>
+                        ))}
+                    </div>
+                    <div className='row-span-2'>
+                        <button onClick={handleMenu} className='w-full h-full flex justify-start items-center cursor-pointer'>
+                            <X className='text-accent text-2xl font-bold h-12 w-12' />
+                        </button>
+                    </div>
+                </div>
                 <div className='bg-bg w-full h-full rounded-xl grid grid-rows-8'>
-                    <div className='row-span-1 flex justify-center items-center'>
-                        <img src={smallLogo} alt="logo" className='object-cover w-40' />
+                    <div className='row-span-1 flex justify-around items-center'>
+                        <div>
+                            <img src={smallLogo} alt="logo" className='object-cover w-40' />
+                        </div>
+                        <div>
+                            <button onClick={handleMenu} className='w-full h-full flex justify-center items-center cursor-pointer'>
+                                <Menu className='text-accent text-2xl font-bold h-12 w-12' />
+                            </button>
+                        </div>
                     </div>
                     <div className='row-span-7 flex flex-wrap justify-center gap-4 p-4 overflow-y-auto'>
                         {[...Array(100)].map((_, i) => (
